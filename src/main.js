73769a6a3f334a2ddae4f4eededddc1ea186efbd
ice <940938974@qq.com>
2018-01-05 00:12:24 +0800

    no message

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import store from './store/store'
+// import'./assets/less/less.less'
 import './rem'
 Vue.config.productionTip = false;
 /* eslint-disable no-new */
